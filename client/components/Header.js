943c97bf61bf4f1077f21285c6e7e81c35d13f55
iwannabekoshka <pokusaeff.daniil@gmail.com>
2021-12-14 09:23:50 +0300

    fix: fix cart text and counter

--- client/components/Header.js
+++ client/components/Header.js
@@ -58,7 +58,7 @@ export default function Header({ cartCounter }) {
                             {/* Nav item */}
                             <Nav.Item>
                                     <Link href="/cart">
-                                        <a>
+                                        <a className="d-flex align-items-center">
                                             <div className="cart">
                                                 <svg className="me-2" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" viewBox="0 0 16 16">
                                                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
@@ -82,7 +82,7 @@ export default function Header({ cartCounter }) {
                 }    
                 .cart-counter {
                     position: absolute;
-                    display: ${cartCounter !== 0 ? 'flex' : 'none'};
+                    display: ${cartCounter > 0 ? 'flex' : 'none'};
                     align-items: center;
                     justify-content: center;
                     bottom: -10px;
