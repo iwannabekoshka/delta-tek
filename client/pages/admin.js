0256e507c64940a01fd57f5769af4cc9b175e1c0
pokusaeff.daniil <69844034+iwannabekoshka@users.noreply.github.com>
2021-12-21 21:11:54 +0300

    feat: add product form (beginning)

--- client/pages/admin.js
+++ client/pages/admin.js
@@ -4,17 +4,22 @@ import {useState} from "react";
 
 export default function Admin() {
     const [authorized, setAuthorized] = useState(false)
+    const [addFormVisible, setAddFormVisible] = useState(false)
 
-    const submitForm = (event) => {
+    const submitFormLogin = (event) => {
         event.preventDefault()
 
         setAuthorized(true)
     }
 
+    const toggleAddForm = () => {
+        setAddFormVisible(prev => !prev)
+    }
+
     return (<>
         <div className="container">
             {!authorized ?
-                <form onSubmit={submitForm} className="w-50 mx-auto">
+                <form onSubmit={submitFormLogin} className="w-50 mx-auto">
                     <div className="mb-3">
                         <label htmlFor="login" className="form-label">Логин</label>
                         <input type="text" className="form-control" id="login"/>
@@ -28,6 +33,31 @@ export default function Admin() {
             :
                 <Tabs defaultActiveKey="goods" className="mb-3">
                     <Tab eventKey="goods" title="Товары">
+                        <button className="btn btn-primary mb-2" onClick={toggleAddForm}>
+                            {!addFormVisible ? 'Добавить' : 'Скрыть форму добавления'}
+                        </button>
+
+                        <form className={`border shadow-sm rounded-2 p-2 bg-white mb-2 ${!addFormVisible && 'collapse'}`}>
+                            <div className="mb-3">
+                                <label htmlFor="text" className="form-label">Наименование</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="title"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="description" className="form-label">Описание</label>
+                                <textarea
+                                    className="form-control"
+                                    id="description"
+                                />
+                            </div>
+                            <div className="d-flex justify-content-end">
+                                <button type="submit" className="btn btn-primary">Добавить</button>
+                            </div>
+                        </form>
+
                         <ul className="list-group shadow-sm rounded-2">
                             <li className="list-group-item">1 Товар</li>
                             <li className="list-group-item">2</li>
