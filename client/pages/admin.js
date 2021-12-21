38c9a47fd33a5c3b07fa26be93e3f94d9459df41
pokusaeff.daniil <69844034+iwannabekoshka@users.noreply.github.com>
2021-12-21 19:57:20 +0300

    feat: add Admin page

---
+++ client/pages/admin.js
@@ -0,0 +1,52 @@
+import {Tab, Tabs} from "react-bootstrap";
+import {useState} from "react";
+
+
+export default function Admin() {
+    const [authorized, setAuthorized] = useState(false)
+
+    const submitForm = (event) => {
+        event.preventDefault()
+
+        setAuthorized(true)
+    }
+
+    return (<>
+        <div className="container">
+            {!authorized ?
+                <form onSubmit={submitForm} className="w-50 mx-auto">
+                    <div className="mb-3">
+                        <label htmlFor="login" className="form-label">Логин</label>
+                        <input type="text" className="form-control" id="login"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Пароль</label>
+                        <input type="password" className="form-control" id="password"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Войти</button>
+                </form>
+            :
+                <Tabs defaultActiveKey="goods" className="mb-3">
+                    <Tab eventKey="goods" title="Товары">
+                        <ul className="list-group">
+                            <li className="list-group-item">1 Товар</li>
+                            <li className="list-group-item">2</li>
+                            <li className="list-group-item">3</li>
+                            <li className="list-group-item">4</li>
+                            <li className="list-group-item">5</li>
+                        </ul>
+                    </Tab>
+                    <Tab eventKey="orders" title="Заказы">
+                        <ul className="list-group">
+                            <li className="list-group-item">1 заказ</li>
+                            <li className="list-group-item">2</li>
+                            <li className="list-group-item">3</li>
+                            <li className="list-group-item">4</li>
+                            <li className="list-group-item">5</li>
+                        </ul>
+                    </Tab>
+                </Tabs>
+            }
+        </div>
+    </>)
+}
